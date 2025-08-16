cc97e1ea3a90d42daae7924c6e83523241136600
MaferGH <mafergariah@gmail.com>
2025-08-16 00:58:17 -0600

    Add files via upload

---
+++ archivo.py
@@ -0,0 +1,11 @@
+#Primer linea, imprimir
+
+print("Hola, mundo!")
+print ("Esto es un saludo.")
+
+#Variables
+
+texto = "Esta es mi variable llamada 'texto'"
+print(texto)
+    
+#Nota:Esta prohibido usar sangria en python
