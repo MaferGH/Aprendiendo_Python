5336e83bfe40748d88645b98683f41f8985f5cc3
MaferGH <mafergariah@gmail.com>
2025-08-17 19:44:39 -0600

    Add files via upload

---
+++ Cadenas.py
@@ -0,0 +1,17 @@
+#Podemos agregar textos y seguir escribiendo si que ocurra errores
+cadena = 'Esta cadena no es erronea \
+abarca varias lineas, \
+y todo ok'
+print(cadena)
+
+
+#Si queremos espacio se pone \n para pasar a otra linea
+cadena = "Aqui esta el primer parrafo \n, este es el segundo \n y este el tercero"
+print(cadena)
+
+#O está otra forma para escribir cadena es poner 3 comillas ''' o """
+cadena = """Aqui
+se supone que seria más facil
+no agregamos diagonal invertida con n
+y todo es más legible"""
+print(cadena)
